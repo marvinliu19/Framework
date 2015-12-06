6642c6d1f9fb94aa27762d664c5a7cf511917e56
Amy <aw545@cornell.edu>
2015-12-06 16:30:24 -0500

    almost there

--- src/cs4620/ray2/shader/Glass.java
+++ src/cs4620/ray2/shader/Glass.java
@@ -43,47 +43,41 @@ public class Glass extends Shader {
 	 */
 	@Override
 	public void shade(Colord outIntensity, Scene scene, Ray ray, IntersectionRecord record, int depth) {
-		// TODO#A7: fill in this function.
 		double n1, n2;
-		Vector3d v = ray.direction.normalize();			// outgoing ray "viewing ray"
+		Vector3d v = ray.origin.clone().sub(record.location).normalize();
+		//Vector3d v = ray.direction.normalize();			// outgoing ray 
 		Vector3d norm = record.normal.clone();
-		
-        // 1) Determine whether the ray is coming from the inside of the surface or the outside.
-		if (v.dot(norm) < 0) {
-			// points in same direction of normal
-			n1 = refractiveIndex;
-			n2 = 1.0;
-			v.mul(-1);
-			//System.out.println("here");
-		} else {
+
+		if (v.dot(norm) > 0) {
+			// points in the same direction as the normal
 			n1 = 1.0;
 			n2 = refractiveIndex;
+		} else {
+			n1 = refractiveIndex;
+			n2 = 1.0;
+			norm.negate();				// pointing in different directions need to flip normal
 		}
 		
-		boolean totalInternal = false;
 		// 2) Determine whether total internal reflection occurs.
-		double theta1 = norm.angle(v);
-		//System.out.println("Theta1" + theta1);
+		boolean totalInternal = false;
+		double theta1 = norm.angle(ray.direction);
 		
 		if (n1 > n2) {
 			// https://en.wikipedia.org/wiki/Total_internal_reflection
 			double critical = Math.asin(n2/n1);		// angle of incidence greater, total internal refl occurs
 			if (theta1 > critical) totalInternal = true;
-			System.out.println("Theta1" + theta1 + " Critical" + critical);
-		} else {
-			System.out.println("n2 can't be greater than n1 by Snell's law");
-		}
-		
-		//System.out.println(totalInternal);
+		} 
 
         // 3) Compute the reflected ray and refracted ray (if total internal reflection does not occur)
         //    using Snell's law and call RayTracer.shadeRay on them to shade them
 		outIntensity.setZero();
-		double R = totalInternal ? 1 : fresnel(norm, v, n1);
+		double R = totalInternal ? 1 : fresnelCalc(norm, v, n1, n2);
 
-		Vector3d reflDir = (norm.clone().sub(v)).mul(2*v.dot(norm));
+		//Vector3d reflDir = (norm.clone().sub(v)).mul(2*v.dot(norm));
+		Vector3d reflDir = norm.clone().sub(ray.direction).mul(2*ray.direction.dot(norm));
 				
 		Ray reflected = new Ray();
+		reflected.makeOffsetRay();
 		reflected.set(record.location, reflDir);
 		
 		Colord reflColour = new Colord(reflDir);
@@ -92,12 +86,16 @@ public class Glass extends Shader {
 		reflColour.mul(R);
 		outIntensity.add(reflColour);
 		
+		
 		if (!totalInternal) {
 			// if we don't have total internal reflection, we have to calculate refraction as well
 			Ray refracted = new Ray();
+			refracted.makeOffsetRay();
 			double theta2 = Math.asin((n1/n2)*Math.sin(theta1));
 			double constant = (n1/n2)*Math.cos(theta1) - Math.cos(theta2);
 			Vector3d refrDir = v.clone().mul(n1/n2).add(norm.clone().mul(constant));
+			
+			
 			refracted.set(record.location, refrDir);
 			
 			Colord refrColour = new Colord(refrDir);
@@ -107,6 +105,22 @@ public class Glass extends Shader {
 		}
 				
 	}
-	
+				
+	protected double fresnelCalc(Vector3d normal, Vector3d outgoing, double n1, double n2) {
+		// Modified fresnel to take in an arbitrary n1 and n2
+		if (normal.dot(outgoing) < 0) {
+			// normal and outgoing in different directions
+			normal.mul(-1.0);
+		}
+		
+		double theta1 = normal.angle(outgoing);
+		double theta2 = Math.asin((n1/n2)*Math.sin(theta1));		// use snell's eqn, with n1 = 1 = air
+
+		double Fp = (n2*Math.cos(theta1) - n1*Math.cos(theta2))/(n2*Math.cos(theta1) + n1*Math.cos(theta2));
+		double Fs = (n1*Math.cos(theta1) - n2*Math.cos(theta2))/(n1*Math.cos(theta1) + n2*Math.cos(theta2));
+		
+		double R = 0.5*(Math.pow(Fp, 2) + Math.pow(Fs,  2));
+		return R;
+	}
 
 }
